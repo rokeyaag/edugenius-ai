84d80fd35a84d278d5cb83e4dbe196923f80c496
rokeyaag <lutforitsolution@gmail.com>
2026-08-22 15:22:05 +0600

    Upload complete 11 chapters and official NCTB textbook PDF viewer for Civics and Citizenship (পৌরনীতি ও নাগরিকতা - মানবিক)

--- src/components/KnowledgeVaultView.jsx
+++ src/components/KnowledgeVaultView.jsx
@@ -20,6 +20,7 @@ import { NCTB_FINANCE_BANKING_CHAPTERS } from '../utils/nctbFinanceBankingData';
 import { NCTB_BUSINESS_ENT_CHAPTERS } from '../utils/nctbBusinessEntData';
 import { NCTB_HISTORY_CHAPTERS } from '../utils/nctbHistoryData';
 import { NCTB_GEOGRAPHY_CHAPTERS } from '../utils/nctbGeographyData';
+import { NCTB_CIVICS_CHAPTERS } from '../utils/nctbCivicsData';
 import { 
   BookMarked, 
   Search, 
@@ -80,6 +81,7 @@ export const NCTB_FULL_BOOK_CHAPTERS_MAP = {
   'business-ent': NCTB_BUSINESS_ENT_CHAPTERS,
   'history-bd': NCTB_HISTORY_CHAPTERS,
   'geography-env': NCTB_GEOGRAPHY_CHAPTERS,
+  'civics-citizenship': NCTB_CIVICS_CHAPTERS,
   'ict': NCTB_ICT_CHAPTERS,
   'ict-6': NCTB_ICT_CHAPTERS.slice(0, 3),
   'ict-7': NCTB_ICT_CHAPTERS.slice(0, 4),
